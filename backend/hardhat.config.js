1eb5f917ed4cae25bb7eab495a484ffde97bd3d4
romrlb <romain.buirette37@gmail.com>
2025-03-29 00:02:33 +0100

    fix: fix private key size

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -4,7 +4,7 @@ require('@nomicfoundation/hardhat-verify')
 require('solidity-coverage')
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || ''
-const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+const PRIVATE_KEY = process.env.PRIVATE_KEY || 'ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
 
 module.exports = {
